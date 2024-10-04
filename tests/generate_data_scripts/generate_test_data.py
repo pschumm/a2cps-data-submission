cbe55551f24e287db2c8fd139b1bca66f1fc49df
J Montgomery Maxwell <jmontgomerymaxwell@Js-MacBook-Pro.local>
2024-10-04 15:13:18 -0500

    add taps pipeline & improve pipeline write paths

--- tests/generate_data_scripts/generate_test_data.py
+++ tests/generate_data_scripts/generate_test_data.py
@@ -126,6 +126,38 @@ def generate_pcs_test():
     return df
 
 
+def generate_taps_test():
+    cols = ['TAPSTobaccoProductScl',
+            'TAPSAlcoholUseMaleScl',
+            'TAPSAlcoholUseFemaleScl',
+            'TAPSDrugUseScl',
+            'TAPSPrescriptionMedUseScl',
+            'TAPSOverallYN']
+
+    guids = pd.read_csv("../../ids/guids.csv")
+
+    df = pd.DataFrame()
+
+
+    df['participant_id'] = list(guids['participant_id'])[0:100]
+    df['date_administered'] = [date(2015, 6, 3)]*100
+    df['sex'] = ['Male', 'Female']*50
+    df['dob'] = [date(1985, 4, 23)]*100
+    df['event'] = ['1']*100
+    df['instance'] = ['1']*100
+
+    for i in range(0, len(cols)):
+        if i == 1:
+            df[cols[i]] = [1, 9]*50
+        elif i == 2:
+            df[cols[i]] = [9, 2] * 50
+        elif i == 5:
+            df[cols[i]] = [randint(0, 1) for p in range(0, 100)]
+        else:
+            df[cols[i]] = [randint(0, 4) for p in range(0, 100)]
+    return df
+
+
 test_df = generate_gad7_test()
 test_df.to_csv('../input_healcde_structure/gad7_test.csv', index=False)
 
@@ -138,9 +170,8 @@ test_df.to_csv('../input_healcde_structure/psd_test.csv', index=False)
 test_df = generate_pcs_test()
 test_df.to_csv('../input_healcde_structure/pcs6_test.csv', index=False)
 
-
-
-
+test_df = generate_taps_test()
+test_df.to_csv('../input_healcde_structure/taps_test.csv', index=False)
 
 print(test_df.head(10))
 #print(np.min(test_df))
